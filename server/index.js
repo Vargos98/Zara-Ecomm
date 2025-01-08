4a32a712f4d7f6230797953f5c1743750f4d0a9b
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2025-01-08 14:34:09 +0530

    Error handling methods

--- server/index.js
+++ server/index.js
@@ -10,6 +10,17 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
+// error handler
+app.use((err,erq,res,next)=>{
+  const status = err.status || 500;
+  const message = err.message || "Something went wrong"
+  return res.status(status).json({
+    success:false,
+    status,
+    message,
+    
+  })
+})
 
 app.get("/", async (req, res) => {
   res.status(200).json({
