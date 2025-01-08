c4fddc1b9c85dbc4e800a5f19a8f5492e328c3b3
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2025-01-08 11:28:11 +0530

    Started designing the frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
-
+import styled ,{ThemeProvider} from 'styled-components'
+import {lightTheme} from './utils/Themes'
+import {BrowserRouter, Route, Routes} from'react-router-dom'
+import Navbar from './components/Navbar';
+const Container = styled.div`
+  width:100%;
+  height:100vh;
+  display:flex;
+  flex-direction:column;
+  background : ${({theme})=>theme.bg};
+  color:${({theme})=> theme.text_primary};
+  overflow-x:hidden;
+  overflow-y:hidden;
+  transition-a:all 0.2s ease;
+`;
 const App = () => {
   return (
-    <div>App</div>
+    <ThemeProvider theme={lightTheme}>
+      <BrowserRouter>
+        <Container>
+          <Navbar/>
+        </Container>
+      </BrowserRouter>
+    </ThemeProvider>
   )
 }
 
